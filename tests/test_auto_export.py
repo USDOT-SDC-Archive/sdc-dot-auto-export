c9323d8629f33fa3f7498404e1d1ae077469ca6f
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2020-03-18 17:27:57 -0600

    addition to test

--- tests/test_auto_export.py
+++ tests/test_auto_export.py
@@ -4,6 +4,7 @@ import sys
 from moto import mock_s3
 from moto import mock_ses
 from moto import mock_dynamodb2
+import botocore.errorfactory
 
 sys.path.append('./dot-sdc-auto-export')
 from dot_sdc_auto_export import lambda_function
@@ -82,5 +83,9 @@ def run_auto_export(file_path):
 @mock_dynamodb2
 def test_auto_export():
     print('-----------------------TEST_AUTO_EXPORT--------------------------')
-    run_auto_export('./tests/testFiles/test')
-    assert True
+    try:
+        run_auto_export('./tests/testFiles/test')
+    except botocore.errorfactory.MessageRejected as e:
+        assert True
+
+    assert False
